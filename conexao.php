95588372814c8b30940e98619be667604e818430
Claydson Vieira <61741188+ClaydsonVieira@users.noreply.github.com>
2023-08-22 16:30:57 -0300

    That's all folks

---
+++ conexao.php
@@ -0,0 +1,87 @@
+<?php 
+
+class Conexao{
+	 private $db;
+
+    public function __construct($servidor, $base, $usuario, $senha)
+    {
+        try {
+            $this->db = new PDO("mysql:host=$servidor;dbname=$base", $usuario, $senha);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Falha na conexão: " . $e->getMessage());
+        }
+    }
+	
+	public function Insert($tabela, $dados)
+    {
+        try {
+            $columns = implode(", ", array_keys($dados));
+            $values = ":" . implode(", :", array_keys($dados));
+
+            $query = "INSERT INTO $tabela ($columns) VALUES ($values)";
+            $stmt = $this->db->prepare($query);
+
+            foreach ($dados as $key => $value) {
+                $stmt->bindValue(":$key", $value);
+            }
+
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+	
+	public function Select($tabela, $filtro)
+    {
+        try {
+            $query = "SELECT * FROM $tabela WHERE :filtro";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(":filtro", $filtro);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+	
+	 public function Update($tabela, $dados, $filtro)
+    {
+        try {
+            $updates = "";
+            foreach ($dados as $key => $value) {
+                $updates .= "$key = :$key, ";
+            }
+            $updates = rtrim($updates, ", ");
+
+            $query = "UPDATE $tabela SET $updates WHERE :filtro";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(":filtro", $filtro);
+
+            foreach ($dados as $key => $value) {
+                $stmt->bindValue(":$key", $value);
+            }
+
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+	
+	 public function Deletar($tabela, $filtro)
+    {
+        try {
+            $query = "DELETE FROM $tabela WHERE :filtro";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(":filtro", $filtro);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
+
+?>
